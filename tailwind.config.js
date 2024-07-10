598dcab4c061510e1684c06005f4b9e797f914a5
Hoang Nguyen <100697734+isoamyl50@users.noreply.github.com>
2024-07-10 14:42:10 +0700

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [],
+  theme: {
+    extend: {},
+  }git remote add origin https://github.com/dangthanhvinh003/VieTicketCloneWithReact.git
+  git branch -M main
+  git push -u origin main,
+  plugins: [],
+}
+
