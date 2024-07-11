fbc8c792ed5c47baaf82fb6a8a11be427d89b607
Hoang Nguyen <100697734+isoamyl50@users.noreply.github.com>
2024-07-11 13:56:01 +0700

    chore: Remove unnecessary code in tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,6 @@ export default {
   content: [],
   theme: {
     extend: {},
-  }git remote add origin https://github.com/dangthanhvinh003/VieTicketCloneWithReact.git
-  git branch -M main
-  git push -u origin main,
-  plugins: [],
+  }
 }
 
