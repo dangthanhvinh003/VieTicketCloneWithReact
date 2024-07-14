28e7c84b44b4c31f5294fa86251c91c6187f714b
lethyanha <lethyanha2004@gmail.com>
2024-07-15 06:58:04 +0700

    view detail

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import { useState } from 'react'
 import {BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Home from './components/landing/home'
 import CreateEvent from './components/createEvent/createEvent'
+import ViewDetail from './components/viewDetail/viewDetail'
+
 
 function App() {
   const [count, setCount] = useState(0)
@@ -14,7 +16,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home/>} />
         <Route path="/createEvent" element={<CreateEvent/>} />
-
+        <Route path="/viewDetail" element={<ViewDetail/>} />
       </Routes>
      </Router>
     </>
