754feeb4f48fd810ed00bcb984e4d20a7a8b8de2
Hoang Nguyen <100697734+isoamyl50@users.noreply.github.com>
2024-07-15 08:23:31 +0700

    refactor: Add "Add event" button for organizers in HomeHeader

--- src/components/home-page/HomeHeader.jsx
+++ src/components/home-page/HomeHeader.jsx
@@ -1,26 +1,41 @@
-import {Link} from "react-router-dom";
-import {LoginOutlined, Search} from "@mui/icons-material";
+import { Link } from "react-router-dom";
+import { Add, LoginOutlined, Search } from "@mui/icons-material";
 import React from "react";
-import {Container} from "react-bootstrap";
+import { Container } from "react-bootstrap";
 import PropTypes from "prop-types";
 import { useAuth } from "../../context/auth";
 
-const HomeHeader = ({commonClassName}) => {
-    const {user} = useAuth()
+const HomeHeader = ({ commonClassName }) => {
+    const { user } = useAuth()
 
     console.log(user)
     return (
         <Container className={commonClassName + ' home-header'}>
             <header data-bs-theme="dark" className={'d-flex justify-content-between align-items-center'}>
-                <h2 className="fs-3" style={{color: 'var(--bs-body-color)'}}>VieTicket</h2>
+                <h2 className="fs-3" style={{ color: 'var(--bs-body-color)' }}>VieTicket</h2>
                 <div>
                     <Link to={"/search"} className={'btn btn-primary'}>
                         <Search className="me-2" />
                         Search Events
                     </Link>
                 </div>
-                <div>
-                    {user ? <div className="text-white">{user.email}</div> : <Link to="/login" className="btn btn-primary"><LoginOutlined/> Login</Link>}
+                <div className="d-flex align-items-center">
+                    {
+                        user && user.role === 'o' &&
+                        (
+                            <Link to={"/createEvent"} className={'btn btn-primary me-2'}>
+                                <Add className="me-2" />
+                                Add event
+                            </Link>
+                        )
+                    }
+                    {user ?
+                        <div className="text-white">
+                            <Link to={`/users/${user.id}`}>
+                                {user.email}
+                            </Link>
+                        </div>
+                        : <Link to="/login" className="btn btn-primary"><LoginOutlined /> Login</Link>}
                 </div>
             </header>
         </Container>
